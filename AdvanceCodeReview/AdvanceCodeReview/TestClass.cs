8265d30895eac7de83eb70da45895f0802a3bde9
AnithaBabu <anithabaabu@gmail.com>
2021-09-24 16:33:37 +0100

    Added review comments

---
+++ AdvanceCodeReview/AdvanceCodeReview/TestClass.cs
@@ -0,0 +1,103 @@
+﻿using System; //CodeReview - This namespace is required to catch the Exception in the catch block. 
+using System.Collections.Generic;
+
+
+namespace ConsoleApp1 //CodeReview - namespace name and class name can be more descriptive for readability
+{
+    
+    using System.IO; //CodeReview2 - This needs to be moved above the namespace for better visibility
+
+    /// &lt;summary&gt;
+    /// Class to read in a csv and parse the values into type
+    /// &lt;/summary&gt;
+    public class TestClass
+    {
+        public string ErrorMessage;//This string is never used, either we can use this string to catch the error in the catch block or remove the string as it is not used
+
+        private object lockA = new object ();//CodeReview - This lock is not required. Usually we use locks when we have multiple threads. A piece of code is locked for thread1 so other threads can wait until the lock is released
+
+        private object lockB = new object ();//CodeReview - This lock is not required for the same reason mentioned above
+
+        //CodeReview -  resultValidator is an object of another class. To make this class more decoupled the object can be injected via GetResult() method
+        private IResultValidator resultValidator = new ResultValidator(); 
+
+        public List<Result> GetResult() 
+        {
+            // Current path to file.
+            var fp = "C:\\Temp\\FileToOpen.csv";//CodeReview - 1. To increase readability fp can be changed to filepath. 2. This path can be moved to configuration file
+
+            var text = File.ReadAllText(fp);//This code needs try catch block to validate the file as there are more possibility for error like file is not found or the file is used by any other source
+
+            List<Result> results = new List<Result>(100);//CodeReview - It's better to let the list grow depending upon the requirements rather than specifying 100
+
+            lock (lockA)//CodeReview - There is no need for a lock
+            {
+                foreach (var l in text.Split(char.Parse(",")))//CodeReview - 1. text.Split(',') can be used instead. 2. The variable 'l' can be more readable
+                {
+                    try
+                    {
+                        if (l.StartsWith("ExcepectedValue"))
+                        {
+                            lock (lockB)//CodeReview - TThere is no need for a lock
+                            {
+                                var newResult = this.GetResult(l); //CodeReview - 'this' keyword can be removed
+                                if (this.resultValidator.IsValid(newResult)) //CodeReview - 'this' keyword can be removed (it can be included when the field name and parameter name matches)
+                                {
+                                    results.Add(newResult);
+                                }
+                            }
+                        }
+                    }
+                    catch (Exception)
+                    {//CodeReview - Need to catch the exception and throw it as a message or log it
+                    }
+                }
+            }
+            return results;
+        }
+
+        public string DescribeResult(Result theResult)
+        {
+            lock (lockB)//CodeReview - There is no need for a lock
+            {
+                lock (lockA)//CodeReview - There is no need for a lock
+                {
+                    return string.Format("{0}{1}", theResult.Key, theResult.Value);
+                }
+            }
+        }
+
+        /*CodeReview - This method is private but not used inside this class, so this method can be removed*/
+        private void AddResult(Result toAdd, List<Result> results)
+        {
+        results.Add(toAdd);
+        }
+
+    public Result GetResult(string input)
+    {
+        Result newResult = new Result();
+        newResult.Key = input.Split('B')[0];//CodeReview - Need to handle other scenarios like when string cannot be split by 'B'
+            newResult.Value = input.Split('B')[1];
+        return new Result();
+    }
+}
+
+public class Result 
+{
+    public string Key { get; set; }
+    public string Value { get; set; }
+}
+
+public interface IResultValidator
+{
+    bool IsValid(Result toValidate);
+}
+
+public class ResultValidator : IResultValidator
+{
+    public bool IsValid(Result toValidate)
+    {
+        return !string.IsNullOrEmpty(toValidate.Value) && !string.IsNullOrEmpty(toValidate.Key);
+    }
+}
+}
